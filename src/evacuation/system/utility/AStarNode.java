d912a5b1f7cef58753186e2bad24b53d76b2649f
Jamal Rahman <jamalrahman95@gmail.com>
2019-03-25 22:56:02 +0000

    Decided to implement AStar using a parallel set of AStarNodes which are linked to the Junctions by a key,value pair

---
+++ src/evacuation/system/utility/AStarNode.java
@@ -0,0 +1,9 @@
+package evacuation.system.utility;
+
+import evacuation.system.Junction;
+
+public class AStarNode {
+    private AStarNode parent;
+    private double f;
+    private double g;
+}
--- src/evacuation/system/utility/AStarSearch.java
+++ src/evacuation/system/utility/AStarSearch.java
@@ -9,16 +9,36 @@ import java.util.HashSet;
 import java.util.PriorityQueue;
 
 public class AStarSearch {
+    private HashMap<Junction,AStarNode> nodes;
+    private Network network;
 
-    public ArrayList<Junction> getRoute(Network network, Junction start, Junction goal){
+    public AStarSearch(Network network){
+        this.network = network;
+
+        for ( Object junction:
+                network.getAllNodes()) {
+            nodes.put((Junction) junction,new AStarNode());
+        }
+
+    }
+
+    public ArrayList<Junction> getRoute(Junction startJunction, Junction goalJunction){
+
         HashSet<Junction> closedSet = new HashSet<>();
         PriorityQueue<Junction> openList = new PriorityQueue<>();
+        
+        AStarNode startNode=nodes.get(startJunction);
+        AStarNode goalNode = nodes.get(goalJunction);
+
+        openList.add(startJunction);
+
+
 
         return null;
     }
 
-    private double manhattanDistance(Junction start, Junction goal){
-        return (Math.abs(start.getLocation().getX()-goal.getLocation().getX())+Math.abs(start.getLocation().getY()-goal.getLocation().getY()));
+    private double manhattanDistance(Junction first, Junction second){
+        return (Math.abs(first.getLocation().getX()-second.getLocation().getX())+Math.abs(first.getLocation().getY()-second.getLocation().getY()));
     }
 
 }
